de8041def32082cb06f2db4bfc9d6fe5661551b1
Vehbi Sinan Tunalioglu <vst@vsthost.com>
2015-01-28 13:15:21 +0800

    Development version bumped to 0.0.10dev

--- __openerp__.py
+++ __openerp__.py
@@ -1,10 +1,10 @@
 {
     "name": "tesa",
-    "version": "0.0.9",
+    "version": "0.0.10dev",
     "author": "Telosoft",
     "category": "Tools",
     "website": "http://www.telosoft.com",
-    "description": """Provides an ERP Implementation and Customization Addon (Version 0.0.9)""",
+    "description": """Provides an ERP Implementation and Customization Addon (Version 0.0.10dev)""",
     "depends": [
         "base",
         "account_accountant",
